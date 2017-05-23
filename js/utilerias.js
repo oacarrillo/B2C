9acb1417eb4fee16a789a1febbada5e4d8e7ff5c
server <automatizacion@qvision.com.co>
2017-05-23 10:21:48 -0500

    login de jason quemado, version 1 del carrito

--- js/utilerias.js
+++ js/utilerias.js
@@ -35,6 +35,123 @@ function buscar(){
     sessionStorage.prodFind ="http://kallsonyproducto.azurewebsites.net/kallsony/productos/busqueda/descripcion/"+porId;
 }
 
+function enviarPost(url,json){
+    alert("Entre");
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onreadystatechange = function () {
+        console.log(xhr.readyState);
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log("entre log");
+            console.log("en str:"+xhr.responseText);
+            var json = JSON.parse(xhr.responseText);
+            console.log(json);
+            //sessionStorage.jsonPost = xhr.responseText;
+            //alert(sessionStorage.jsonPost);
+            //console.log(sessionStorage.jsonPost);
+        }
+    };
+    var data = json;//JSON.stringify({"email": "hey@mail.com", "password": "101010"});
+    xhr.send(data);
+}
+
+//cierra sesion de usuario
+function sesion() {
+    sessionStorage.jsonPost="";
+    sessionStorage.tokenUser="";
+    location.reload();
+}
+
+//Login de usuario
+function login() {
+    var user=document.getElementById("loginuser").value;
+    var pass=document.getElementById("loginpass").value;
+    if(user.length>0 && pass.length>0){
+        var text = '{"username": "oscarcarrillo","password": 123456}';
+        var url="http://35.184.218.7:8080/SERVICES_OMS/loginService/validarLogin";
+        //enviarPost(url,text);
+/*
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-type", "application/json");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                //var json = JSON.parse(xhr.responseText);
+                var json = xhr.responseText;
+                console.log(json);
+            }
+        };
+        var data = JSON.stringify(text);
+        xhr.send(data);
+*/
+        var text='{'+
+          '"id": 0,'+
+          '"password": "LvxBN5EW7bg=",'+  
+          '"idUsuario": null,'+
+          '"tipoUsuario": null,'+  
+          '"estado": null,'+
+          '"username": "oscarcarrillo",'+
+          '"ownerid": null,'+
+          '"token": "S8zCNuZWKTc="'+
+        '}';
+
+        //console.log(json);
+        sessionStorage.jsonPost=text;
+        location.reload();
+    }
+    else{
+        alert("Por favor ingrese los datos de acceso");       
+    }
+
+ }
+
+//Muestra form login modal
+ function showLogin(){
+    $("#id01").css("display", "block");
+}
+
+
+//carga perfil de cliente al iniciar la pagina
+function load(){
+    
+    //$("#lilogin").css("display", "none");
+    //$("#liorden").css("display", "block");
+    console.log(sessionStorage.tokenUser);
+    if(sessionStorage.jsonPost){
+        console.log("Con sesion");      
+        var obj = JSON.parse(sessionStorage.jsonPost);
+        var textlogin2="";
+        textlogin2+="<li id='lisesion' style='display: inline'><a href='javascript:sesion();'>Cerrar Sesión</a></li>";
+        textlogin2+="<li id='liorden' style='display: inline'><a href='#''>Mis Ordenes</a></li>";
+        $("#concar").css("display", "block");//carrito de compras muestra
+        $("#addcar").css("display", "block"); ////agregar al carrito muestra
+        $("#divLogin").html(textlogin2);     
+        $("#nameusuario").html(obj.username);
+        sessionStorage.tokenUser=obj.token;
+    }
+    else{
+        console.log("Sin sesion");
+        var textlogin="";
+        $("#concar").css("display", "none");//carrito de compras oculta
+        $("#addcar").css("display", "none"); //agregar al carrito oculto  
+        textlogin+="<li id='liregistro' style='display: inline'><a href='register.html'>Registro</a></li>";    
+        textlogin+="<li id='lilogin' style='display: inline'><a href='javascript:showLogin();'>Login</a></li>";
+        $("#divLogin").html(textlogin);        
+    }
+
+    //Ventana modal del login
+    var modal = document.getElementById('id01');
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }  
+
+}
+
+
 
 
 
